b827940a84de47fb98d1b1e4bd6c4c43ddcb20bb
Jairo Espinel <jairo25509@hotmail.com>
2021-08-20 22:08:29 -0500

    Diseño del formulario listo. Falta funcionalidad.

---
+++ form-validator/app.js
@@ -0,0 +1,13 @@
+/*
+Verificar el tamaño del username.
+Verificar que el correo no sea de 2-minute mail?
+Verificar que los passwords cuplan los requisitos.
+Verificar que los passwords sean iguales. */
+
+const form = document.getElementById("form");
+
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault;
+  console.log(e.type);
+  console.log(e.defaultPrevented);
+});
