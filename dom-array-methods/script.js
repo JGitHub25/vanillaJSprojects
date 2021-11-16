829e1ee09e9f88adab5891a16eb88db00f20e37e
jpeg25 <jairo25509@hotmail.com>
2021-11-16 18:19:41 -0500

    Users rendered on DOM loaded and click 'Add User'.

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -1,4 +1,4 @@
-const main = document.getElementById("main");
+const mainElem = document.getElementById("main");
 const addUserBtn = document.getElementById("add-user");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const doubleBtn = document.getElementById("double");
@@ -7,7 +7,7 @@ const totalWealthBtn = document.getElementById("calculate-wealth");
 
 let users = [];
 
-async function getUsers() {
+async function getUser() {
   try {
     const response = await fetch("https://randomuser.me/api");
 
@@ -18,8 +18,11 @@ async function getUsers() {
         name: `${user.name.title} ${user.name.first} ${user.name.last}`,
         wealth: Math.floor(Math.random() * 1000000),
       };
+      filteredUser.wealth = filteredUser.wealth
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,");
       users.push(filteredUser);
-      return;
+      return users;
     }
 
     throw new Error("Request failed to fetch the user.");
@@ -28,8 +31,24 @@ async function getUsers() {
   }
 }
 
-getUsers();
-getUsers();
-getUsers();
+async function renderUser() {
+  const data = await getUser();
+  mainElem.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+  data.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong>$${item.wealth}`;
+    mainElem.append(element);
+  });
+}
 
-console.log(`The users are:`, users);
+document.addEventListener("DOMContentLoaded", () => {
+  getUser();
+  getUser();
+  renderUser();
+  console.log(`The users are:`, users);
+});
+addUserBtn.addEventListener("click", () => {
+  renderUser();
+  console.log(`The users are:`, users);
+});
